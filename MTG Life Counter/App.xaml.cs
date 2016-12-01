f5510cd001067963db43ad93b6f7d5bb4284a10d
ExXoDuSs <profexxus@hotmail.com>
2016-12-01 19:29:49 +0200

    Add project files.

---
+++ MTG Life Counter/App.xaml.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace MTG_Life_Counter
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        public int lifePointsA { get; set; }
+        public int lifePointsB { get; set; }
+        public int resetPoints { get; set; }
+        public int poisonCountersA { get; set; }
+        public int poisonCountersB { get; set; }
+        public int winsA { get; set; }
+        public int winsB { get; set; }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+                rootFrame.Navigated += OnNavigated;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+
+                // Register a handler for BackRequested events and set the
+                // visibility of the Back button
+                SystemNavigationManager.GetForCurrentView().BackRequested += OnBackRequested;
+
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    rootFrame.CanGoBack ?
+                    AppViewBackButtonVisibility.Visible :
+                    AppViewBackButtonVisibility.Collapsed;
+
+                if (ApiInformation.IsTypePresent("Windows.UI.ViewManagement.ApplicationView"))
+                {
+                    var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+                    if (titleBar != null)
+                    {
+                        titleBar.ButtonBackgroundColor = GetSolidColorBrush("#FF3B473F").Color;
+                        titleBar.ButtonForegroundColor = Colors.White;
+                        titleBar.BackgroundColor = GetSolidColorBrush("#FF3B473F").Color;
+                        titleBar.ForegroundColor = Colors.White;
+                    }
+                }
+
+                //Initialization of HP values
+                lifePointsA = 20;
+                lifePointsB = 20;
+                resetPoints = 20;
+                poisonCountersA = 0;
+                poisonCountersB = 0;
+
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(MatchPage), e.Arguments);
+                }
+                // Ensure the current window is active
+                Window.Current.Activate();
+            }
+        }
+        //Function to convert Hex to Color
+        public SolidColorBrush GetSolidColorBrush(string hex)
+        {
+            hex = hex.Replace("#", string.Empty);
+            byte a = (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
+            byte r = (byte)(Convert.ToUInt32(hex.Substring(2, 2), 16));
+            byte g = (byte)(Convert.ToUInt32(hex.Substring(4, 2), 16));
+            byte b = (byte)(Convert.ToUInt32(hex.Substring(6, 2), 16));
+            SolidColorBrush myBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(a, r, g, b));
+            return myBrush;
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private void OnNavigated(object sender, NavigationEventArgs e)
+        {
+            // Each time a navigation event occurs, update the Back button's visibility
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                ((Frame)sender).CanGoBack ?
+                AppViewBackButtonVisibility.Visible :
+                AppViewBackButtonVisibility.Collapsed;
+        }
+
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+
+        private void OnBackRequested(object sender, BackRequestedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame.CanGoBack)
+            {
+                e.Handled = true;
+                rootFrame.GoBack();
+            }
+        }
+    }
+}
---
+++ MTG Life Counter/MatchPage.xaml.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MTG_Life_Counter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MatchPage : Page
+    {
+
+        public MatchPage()
+        {
+            this.InitializeComponent();            
+
+            var lifeObj = App.Current as App;
+
+            player1HP.Text = lifeObj.lifePointsA.ToString();
+            player2HP.Text = lifeObj.lifePointsB.ToString();
+
+            poisonTextA.Text = lifeObj.poisonCountersA.ToString();
+            poisonTextB.Text = lifeObj.poisonCountersB.ToString();
+
+        }
+
+
+
+        // player 1 HP modification events
+        private void sub1a_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+            lifeObj.lifePointsA--;            
+
+            player1HP.Text = lifeObj.lifePointsA.ToString();
+
+        }
+
+        private void add1a_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+            lifeObj.lifePointsA++;
+            
+            player1HP.Text = lifeObj.lifePointsA.ToString();
+        }
+
+        private void sub5a_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+            lifeObj.lifePointsA = lifeObj.lifePointsA - 5;
+
+            player1HP.Text = lifeObj.lifePointsA.ToString();
+        }
+
+        private void add5a_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+            lifeObj.lifePointsA = lifeObj.lifePointsA + 5;
+
+            player1HP.Text = lifeObj.lifePointsA.ToString();
+        }
+
+        //player 2 HP modification events
+        private void sub1b_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+            lifeObj.lifePointsB--;
+
+            player2HP.Text = lifeObj.lifePointsB.ToString();
+        }
+
+        private void add1b_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+            lifeObj.lifePointsB++;
+
+            player2HP.Text = lifeObj.lifePointsB.ToString();
+        }
+
+        private void sub5b_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+            lifeObj.lifePointsB = lifeObj.lifePointsB - 5;
+
+            player2HP.Text = lifeObj.lifePointsB.ToString();
+        }
+
+        private void add5b_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+            lifeObj.lifePointsB = lifeObj.lifePointsB + 5;
+
+            player2HP.Text = lifeObj.lifePointsB.ToString();
+        }
+
+        //poison
+        //player 1
+        private void subPoisA_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+
+            lifeObj.poisonCountersA--;
+            if (lifeObj.poisonCountersA <= 0)
+            {
+                lifeObj.poisonCountersA = 0;
+            }
+            poisonTextA.Text = lifeObj.poisonCountersA.ToString();
+        }
+
+        private void addPoisA_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+
+            lifeObj.poisonCountersA++;
+            poisonTextA.Text = lifeObj.poisonCountersA.ToString();
+        }
+
+        //player 2
+        private void subPoisB_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+
+            lifeObj.poisonCountersB--;
+            if (lifeObj.poisonCountersB <= 0)
+            {
+                lifeObj.poisonCountersB = 0;
+            }
+            poisonTextB.Text = lifeObj.poisonCountersB.ToString();
+        }
+
+        private void addPoisB_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+
+            lifeObj.poisonCountersB++;
+            poisonTextB.Text = lifeObj.poisonCountersB.ToString();
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            var lifeObj = App.Current as App;
+            lifeObj.lifePointsA = lifeObj.resetPoints;
+            lifeObj.lifePointsB = lifeObj.resetPoints;
+            lifeObj.poisonCountersA = 0;
+            lifeObj.poisonCountersB = 0;
+            player1HP.Text = lifeObj.resetPoints.ToString();
+            player2HP.Text = lifeObj.resetPoints.ToString();
+            poisonTextA.Text = 0.ToString();
+            poisonTextB.Text = 0.ToString();
+
+        }
+
+        private void img1_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeBackgroundImage("Assets/bg_1.jpg");
+        }
+
+        private void img2_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeBackgroundImage("Assets/bg_2.jpg");
+        }
+
+        private void img3_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeBackgroundImage("Assets/bg_3.jpg");
+        }
+
+        private void img4_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeBackgroundImage("Assets/bg_4.jpg");
+        }
+
+        private void img5_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeBackgroundImage("Assets/bg_5.jpg");
+        }
+
+        private void appBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.ApplicationModel.DataTransfer.DataTransferManager.ShowShareUI();
+            Windows.ApplicationModel.DataTransfer.DataTransferManager.GetForCurrentView().DataRequested += MainPage_DataRequested;
+        }
+
+        void MainPage_DataRequested(Windows.ApplicationModel.DataTransfer.DataTransferManager sender, Windows.ApplicationModel.DataTransfer.DataRequestedEventArgs args)
+        {
+            //ATENTION!!! CHANGE THE URI WHEN YOU OBTAIN THE STORE ID OF THIS APP!
+            Uri URI = new Uri("https://www.microsoft.com/store/apps/9NBLGGH526XJ");
+            args.Request.Data.SetWebLink(URI);
+            args.Request.Data.Properties.Title = "MTG Simple Life Counter";
+            args.Request.Data.Properties.Description = "This application will keep the score and current situation of Magic The Gathering match.";
+        }
+
+        private void infoAppButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(info));
+        }
+
+        private void HP20InitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AlertBox(20);
+        }
+
+        private void HP40InitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AlertBox(40);
+        }
+
+
+
+        // Custom Methods ------------------<-<-<-<
+
+        private async void AlertBox(int hpValue)
+        {
+            ContentDialog noWifiDialog = new ContentDialog()
+            {
+                Title = "Start New Match?",
+                Content = "This will reset the values. Continue?",
+                PrimaryButtonText = "No",
+                SecondaryButtonText = "Yes"
+            };
+
+            ContentDialogResult result = await noWifiDialog.ShowAsync();
+
+            if (result == ContentDialogResult.Secondary)
+            {
+                var lifeObj = App.Current as App;
+
+                lifeObj.lifePointsA = hpValue;
+                lifeObj.lifePointsB = hpValue;
+
+                player1HP.Text = lifeObj.lifePointsA.ToString();
+                player2HP.Text = lifeObj.lifePointsB.ToString();
+
+                lifeObj.resetPoints = hpValue;
+            }
+        }
+
+        private void ChangeBackgroundImage(string imgUri)
+        {
+            main.Background = new ImageBrush { ImageSource = new BitmapImage(new Uri(this.BaseUri, imgUri)), Stretch = Stretch.Fill };
+        }
+    }
+}
